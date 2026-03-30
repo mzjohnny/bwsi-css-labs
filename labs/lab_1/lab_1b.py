c40f547598b7a2fd88863961c58155c91b9bbb7f
mzjohnny <mzjohnnyli114@gmail.com>
2026-03-30 09:39:02 +0800

    add user input santization

--- labs/lab_1/lab_1b.py
+++ labs/lab_1/lab_1b.py
@@ -37,6 +37,20 @@ def simple_calculator(operation: str, num1: float, num2: float) -> float:
     else:
         raise ValueError("Invalid operation. Please choose from 'add', 'subtract', 'multiply', or 'divide'.")
 
+def request_santized_number(prompt: str) -> float:
+   """
+   Function to request and santinize user input for operation
+
+   Returns:
+        float: The santinized numric input by the user.
+   """ 
+   while True:
+       try:
+           number = float(input(prompt))
+           return number
+       except ValueError:
+           print(f"Invalid input: Please enter a valid number")
+
 def main():
     
     print(f"===== Simple Calculator =====")
